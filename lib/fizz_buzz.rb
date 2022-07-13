3d56a3b74669dab0c5c1629b5c4165543d074ef5
nyh421 <nyh1218421@gmail.com>
2022-07-13 20:11:02 +0900

    第3章

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,28 @@
+def fizz_buzz(n)
+  if n % 15 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
+  else
+    n.to_s
+  end
+end
+
+require "minitest/autorun"
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal "1", fizz_buzz(1)
+    assert_equal "2", fizz_buzz(2)
+    assert_equal "Fizz", fizz_buzz(3)
+  end
+end
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
